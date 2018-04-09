120169573ea091bb6967186651c8975c4e93ec5f
Le Cong Vien <vienlc@dfm-engineering.com>
2018-04-09 14:35:11 +0700

    update multilanguage

---
+++ resources/views/vendor/voyager/multilingual/language-switcher.blade.php
@@ -0,0 +1,6 @@
+<div class="language-selector" style="margin-top: 19px; position: relative;">
+    <div class="btn-group btn-group-sm">
+        <a href="{{ url('/locale/en') }}"><img src="{{ asset('/images/en.png') }}" style="width: 30px;"></a>
+        <a href="{{ url('/locale/vi') }}"><img src="{{ asset('/images/vn.png') }}" style="width: 30px;"></a>
+    </div>
+</div>
